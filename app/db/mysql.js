1e5f0394fa9f65b5cf560d08c7fc252506af8734
Willem Liu <willem_liu@hotmail.com>
2016-01-25 13:49:25 +0100

    Update mysql.js

--- app/db/mysql.js
+++ app/db/mysql.js
@@ -5,7 +5,7 @@ console.log("Initialize MySQL");
 var mysql = require('mysql');
 
 var pool = mysql.createPool({
-  connectionLimit: 10, // Default 10
+  connectionLimit: 100, // Default 10
   host : "127.0.0.1",
   database: "willim_roster",
   user : "willim_roster",
