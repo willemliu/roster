b7d8af249a44e625d217a7141c1845ce348239fb
Willem Liu <willem_liu@hotmail.com>
2017-03-23 17:20:44 +0100

    Daylight saving fix.

--- js/app/roster/recurdate.js
+++ js/app/roster/recurdate.js
@@ -100,10 +100,32 @@ define([
         if((weekOffset%weekInterval) === 0 && $.inArray(dt.getDay(), days) != -1) {
           result.push(new Date(dt));
         }
-        dt.setDate(dt.getDate()+1); // Increment 1 day
+        dt.setDate(this.addDays(dt.getDate(), 1)); // Increment 1 day
         firstRun = false;
       }
       return result;
+    },
+    
+    /**
+     * Add day considering daylight saving.
+     */
+    addDays: function(date, amount) {
+      var tzOff = date.getTimezoneOffset() * 60 * 1000,
+          t = date.getTime(),
+          d = new Date(),
+          tzOff2;
+
+      t += (1000 * 60 * 60 * 24) * amount;
+      d.setTime(t);
+
+      tzOff2 = d.getTimezoneOffset() * 60 * 1000;
+      if (tzOff != tzOff2) {
+        var diff = tzOff2 - tzOff;
+        t += diff;
+        d.setTime(t);
+      }
+
+      return d;
     },
     
     getInstance: function() {
